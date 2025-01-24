ed62769f6643d4cc3745bda47008a53a11a7d4a0
Youssef Ragab <ragab.youssef95@gmail.com>
2025-01-24 12:58:50 -0500

    add font awesome and fix routing bug

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
+  <Navbar />
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import { RouterLink, RouterView } from "vue-router";
+</script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Example Navbar</h1>
+  <font-awesome-icon :icon="['fas', 'phone']" />
+</template>
+
+<script setup>
+// export default {
+//   name: "Navbar",
+// };
+</script>
