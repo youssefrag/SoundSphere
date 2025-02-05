cb7fa21820eaace8b2a514ab455e4cb70598e9fa
Youssef Ragab <ragab.youssef95@gmail.com>
2025-02-05 17:29:59 -0500

    style artists section

--- client/src/components/ArtistCard.vue
+++ client/src/components/ArtistCard.vue
@@ -2,18 +2,28 @@
   <div
     class="h-[488px] w-[650px] bg-center bg-cover rounded-2xl"
     :class="{
-      'bg-[url(@/assets/images/artist-bg1.png)]': index % 3 == 0,
-      'bg-[url(@/assets/images/artist-bg2.png)]': index % 3 == 1,
-      'bg-[url(@/assets/images/artist-bg3.png)]': index % 3 == 2,
+      'bg-[url(@/assets/images/artist-bg1.avif)]': index % 3 == 0,
+      'bg-[url(@/assets/images/artist-bg2.avif)]': index % 3 == 1,
+      'bg-[url(@/assets/images/artist-bg3.avif)]': index % 3 == 2,
     }"
   >
-    <div class="h-[100%] w-[100%] bg-[#000] opacity-85 rounded-2xl"></div>
+    <div
+      class="h-[100%] w-[100%] bg-[#000] opacity-85 rounded-2xl overflow-x-auto py-[30px] px-[24px]"
+    >
+      <div>
+        <!-- <img :src="require(artist.pictureUrl)" alt="artist-picture" /> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 const { artist, index } = defineProps(["index", "artist"]);
+// import require from "vue";
 
-console.log({ index, artist });
-console.log(index % 3);
+const imagePath = () => {
+  if (artist.pictureUrl !== "") {
+    return require(artist.pictureUrl);
+  }
+};
 </script>
