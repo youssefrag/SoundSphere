d29937ffd933e590a8b35085721a88eb936dcdac
Youssef Ragab <ragab.youssef95@gmail.com>
2025-07-04 13:23:31 -0400

    include image url in saving user

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,32 +1,59 @@
 package models
 
-import (
-	"github.com/youssefrag/SoundSphere/db"
-)
+import "github.com/youssefrag/SoundSphere/db"
 
 type User struct {
 	ID int64
-	Name string
-	Email string
-	Password string
+	Name string `binding:"required"`
+	Email string `binding:"required"`
+	Password string `binding:"required"`
+	ImageUrl string
 }
 
 var users = []User{}
 
 func (u *User) Save() error {
-  // 1) Use Postgres placeholders ($1, $2, $3)
-  // 2) Add RETURNING id so we can grab the new primary key
-  const query = `
-		INSERT INTO users(name, email, password)
-		VALUES ($1, $2, $3)
-		RETURNING id;
-	`
 
-  // QueryRow + Scan is the simplest way to get the new id
-  return db.DB.
-    QueryRow(query, u.Name, u.Email, u.Password).
-    Scan(&u.ID)
+	var (
+		query string
+		args []interface{}
+	)
+	
+	args = []interface{}{u.Name, u.Email, u.Password}
+
+	if u.ImageUrl != "" {
+		// include image_url
+		query = `
+			INSERT INTO users (name, email, password, image_url)
+			VALUES ($1, $2, $3, $4)
+			RETURNING id;
+		`
+
+		args = append(args, u.ImageUrl)
+	} else {
+		query = `
+			INSERT INTO users (name, email, password)
+			VALUES ($1, $2, $3)
+			RETURNING id;
+    `
+  }
+
+	return db.DB.QueryRow(query, args...).Scan(&u.ID)
 }
+
+
+// func (u *User) Save() error {
+
+//   const query = `
+// 		INSERT INTO users(name, email, password)
+// 		VALUES ($1, $2, $3)
+// 		RETURNING id;
+// 	`
+
+//   return db.DB.
+//     QueryRow(query, u.Name, u.Email, u.Password).
+//     Scan(&u.ID)
+// }
 
 func GetAllUsers() []User {
  return users
