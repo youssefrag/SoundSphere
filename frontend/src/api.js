d3d5a8cd47ae3e3247eaaf64948f14108b665531
Youssef Ragab <ragab.youssef95@gmail.com>
2025-07-16 18:23:08 -0400

    serve static file in dockerfile

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_APP_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
